8f87435b516ddb123c950c9a87da51d8ef72d2bc
Jenushan44 <jenushan447@gmail.com>
2024-07-08 18:32:30 -0400

    inputs added

--- calculator.cpp
+++ calculator.cpp
@@ -3,9 +3,21 @@ using namespace std;
 
 int main() {
     
-    cout << "Welcome to the calculator";
+    double num1 = 0;
+    double num2 = 0;
+    double sum = 0;
+    char op = '+';
+
+    cout << "Welcome to the calculator" << endl;
     
+    cout << "Please enter your first value: ";
+    cin >> num1;
     
-    
+    cout << "Please enter the operator: ";
+    cin >> op;
+
+    cout << "Please enter your second value: ";
+    cin >> num2;
+
     return 0;
 }
