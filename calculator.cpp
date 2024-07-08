b1f2103f4ff97e3094f7573292791d719e6bad62
Jenushan44 <jenushan447@gmail.com>
2024-07-08 18:56:20 -0400

    Input validation loop added

--- calculator.cpp
+++ calculator.cpp
@@ -6,15 +6,28 @@ int main() {
     double num1 = 0;
     double num2 = 0;
     double sum = 0;
+    string option = "";
     char op = '+';
+    bool validOperator = false;
+
 
     cout << "Welcome to the calculator" << endl;
     
     cout << "Please enter your first value: ";
     cin >> num1;
     
-    cout << "Please enter the operator: ";
+    while (!validOperator){
+        
+    cout << "Please enter the operator: " <<endl;
     cin >> op;
+
+    if (op == '+' || op == '-' || op == '*' || op == '/') {
+        validOperator = true;
+    } else {
+        cout << "Please enter a valid operator" <<endl;
+    }
+
+    }
 
     cout << "Please enter your second value: ";
     cin >> num2;
@@ -27,7 +40,10 @@ int main() {
         sum = num1 * num2;
     } else if (op == '/') {
         sum = num1 / num2;
-    }
+    } 
+
+    
+
 
     cout << sum;
 
